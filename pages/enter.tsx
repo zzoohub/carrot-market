852768300d1a7b9a62f0f6beaf210fb817074ca2
joyboy10 <csbt1033@gmail.com>
2022-05-30 01:38:15 +0900

    tailwind

---
+++ pages/enter.tsx
@@ -0,0 +1,116 @@
+import { useState } from "react";
+
+export default function Enter() {
+  function calsses(...classNames: string[]) {
+    return classNames.join(" ");
+  }
+
+  const [method, setMethod] = useState<"email" | "phone">("email");
+  const onEmailClick = () => setMethod("email");
+  const onPhoneClick = () => setMethod("phone");
+  return (
+    <div>
+      <h3 className="text-orange-500 font-bold text-2xl text-center mt-5">
+        Enter to Carrot
+      </h3>
+      <div className="px-5">
+        <div>
+          <h5 className="text-center mt-5 text-gray-500 font-light text-sm">
+            Enter using:
+          </h5>
+          <div className="flex justify-between">
+            <button
+              onClick={onEmailClick}
+              className={calsses(
+                "text-center w-full text-m border-b-2 transition focus:outline-none",
+                method === "email"
+                  ? "border-b-orange-400 py-2 text-orange-500 font-medium"
+                  : "border-b-transparent"
+              )}
+            >
+              Email
+            </button>
+            <button
+              onClick={onPhoneClick}
+              className={calsses(
+                "text-center w-full text-m border-b-2 transition focus:outline-none",
+                method === "phone"
+                  ? "border-b-orange-500 py-2 text-orange-500 font-medium"
+                  : "border-b-transparent"
+              )}
+            >
+              Phone
+            </button>
+          </div>
+        </div>
+        <form className="mt-5">
+          <label className="text-xs">
+            {method === "email" ? "Email address" : null}
+            {method === "phone" ? "Phone number" : null}
+          </label>
+          <div>
+            {method === "email" ? (
+              <input
+                type="email"
+                required
+                className="w-full mt-1 border rounded-md border-gray-300 focus:outline-none p-2 focus:border-orange-500"
+              />
+            ) : null}
+            {method === "phone" ? (
+              <div className="flex mt-1">
+                <span className="border rounded-l-md border-r-0 p-2 border-gray-300 text-sm bg-gray-100 select-none">
+                  +82
+                </span>
+                <input
+                  className="p-2 border border-gray-300 rounded-l-none rounded-md w-full focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                  type="number"
+                  required
+                />
+              </div>
+            ) : null}
+          </div>
+          <button className="w-full mt-5 bg-orange-500 text-white font-medium text-md py-2 rounded-md shadow-md">
+            {method === "email" ? "Get login link" : null}
+            {method === "phone" ? "Get one-time password" : null}
+          </button>
+        </form>
+        <div>
+          <div className="relative">
+            <div className="border border-gray-100 mt-8" />
+            <div className="absolute text-center w-full top-[-14px] text-gray-500">
+              <span className="bg-white px-2 select-none text-xs">
+                Or enter with
+              </span>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-2 mt-5">
+            <button className="flex justify-center items-center py-3 w-full bg-gray-100 rounded-md gap-2 text-gray-600 hover:bg-gray-200 shadow-md">
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+              </svg>
+            </button>
+            <button className="flex justify-center items-center py-3 w-full bg-gray-100 rounded-md gap-2 text-gray-600 hover:bg-gray-200 shadow-md">
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
