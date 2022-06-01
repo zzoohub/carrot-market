e733f82c526cc7777ecdd96cbcbdc1ed7913490f
joyboy10 <csbt1033@gmail.com>
2022-06-02 00:37:00 +0900

    ui going

---
+++ pages/components/button.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps {
+  text: string;
+  [key: string]: any;
+}
+export default function Button({ text, ...rest }: ButtonProps) {
+  return (
+    <button
+      {...rest}
+      className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none"
+    >
+      {text}
+    </button>
+  );
+}
