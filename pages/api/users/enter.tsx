31dddb7edd26e859383a04ef9a67474de715d32e
joyboy10 <csbt1033@gmail.com>
2022-06-10 01:57:45 +0900

    good

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,37 +6,6 @@ import withHandler, { ResponseType } from "../withHandler";
 import nodemailer from "nodemailer";
 import mgTransport from "nodemailer-mailgun-transport";
 
-const sendMail = (email: any) => {
-  const options = {
-    auth: {
-      api_key: process.env.MAILGUN_API!,
-      domain: process.env.MAILGUN_DOMAIN,
-    },
-  };
-  const client = nodemailer.createTransport(mgTransport(options));
-  return client
-    .sendMail(email)
-    .then(() => {
-      console.log("Message sent!");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-const sendSecretMail = (address: string, secret: string) => {
-  const emailData = {
-    from: process.env.MY_EMAIL,
-    to: address,
-    subject: "Login Secret for Prismagram 🚀",
-    html: `<h1>hello! your login secret is ${secret}.</h1>
-  <h2>Copy paste on the web/app to Login</h2>`,
-  };
-  console.log(emailData);
-  return sendMail(emailData);
-};
-// mailgun
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
@@ -44,7 +13,6 @@ async function handler(
   const { email, phone } = req.body;
   const user = phone ? { phone: +phone } : { email };
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
-  const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
   const token = await client.token.create({
     data: {
@@ -60,23 +28,6 @@ async function handler(
         },
       },
     },
-  });
-  if (phone) {
-    const message = await twilioClient.messages.create({
-      messagingServiceSid: process.env.TWILIO_MSID,
-      to: process.env.MY_PHONE!,
-      body: `Your login token is ${payload}`,
-    });
-    console.log(message);
-  } else if (email) {
-    const sendEmail = await sendSecretMail(process.env.MY_EMAIL!, "test words")
-      .then((result: any) => console.log(`result is ${result}`))
-      .catch((err: any) => console.log(`error is ${err}`));
-  }
-
-  console.log(token);
-  return res.json({
-    ok: true,
   });
 }
 
