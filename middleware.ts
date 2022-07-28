3561fdbd70bda69cc6393f0cfbcafbf1866e7851
joyboy10 <csbt1033@gmail.com>
2022-07-29 01:26:16 +0900

    fixed

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,21 @@
 // middleware.ts
-import type { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  // const cookie = request.cookies.get("carrotsession");
-  // console.log("pathname", request.nextUrl.pathname);
-  // if (request.nextUrl.pathname !== "/enter") {
-  //   return NextResponse.redirect(
-  //     new URL(`${request.nextUrl.origin}/enter`, request.url)
-  //   );
-  // }
+export function middleware(request: NextRequest, response: NextResponse) {
+  if (!request.nextUrl.pathname.startsWith("/api")) {
+    if (
+      request.cookies.get("carrotsession") === undefined &&
+      request.nextUrl.pathname !== "/enter"
+    ) {
+      console.log("redirect");
+      return NextResponse.redirect(
+        new URL(`${request.nextUrl.origin}/enter`, request.url)
+      );
+    }
+  }
 }
+
+export const config = {
+  matcher: ["/"],
+};
